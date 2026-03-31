4918f51775d1ec207c47ce37839981d2b295d0d4
Paul ALNET <alnet@insa-toulouse.fr>
2026-03-31 10:23:08 +0200

    feat: interpreter runs code

--- interpreter.c
+++ interpreter.c
@@ -1,5 +1,80 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 #include "asm_table.h"
+#define MEMORY_SIZE 16
+
+static void assert_params_in_bounds(struct asm_instr* instr, int count) {
+  uint32_t faulty_address = 0;
+  if (count >= 1 && instr->arg0 >= MEMORY_SIZE) faulty_address = instr->arg0;
+  if (count >= 2 && instr->arg1 >= MEMORY_SIZE) faulty_address = instr->arg1;
+  if (count >= 3 && instr->arg2 >= MEMORY_SIZE) faulty_address = instr->arg2;
+
+  if (faulty_address) {
+    fprintf(stderr, "segfault: out of bounds memory access 0x%x\n", faulty_address);
+    exit(1);
+  }
+}
+
+static int run(struct asm_table* table) {
+  uint32_t memory[MEMORY_SIZE];
+  uint32_t pc = 0;
+  struct asm_instr* instr;
+
+  while (pc != table->instructions_count) {
+    if (pc > table->instructions_count) {
+      fprintf(stderr, "segfault: attempting to read instruction beyond code: pc=0x%x\n", pc);
+      return 1;
+    }
+    instr = &table->instructions[pc];
+    pc++;
+
+    switch (instr->op) {
+      case ASM_ADD:
+        assert_params_in_bounds(instr, 3);
+        memory[instr->arg0] = memory[instr->arg1] + memory[instr->arg2];
+        break;
+      case ASM_MUL:
+        assert_params_in_bounds(instr, 3);
+        memory[instr->arg0] = memory[instr->arg1] * memory[instr->arg2];
+        break;
+      case ASM_SOU:
+        assert_params_in_bounds(instr, 3);
+        memory[instr->arg0] = memory[instr->arg1] - memory[instr->arg2];
+        break;
+      case ASM_DIV:
+        assert_params_in_bounds(instr, 3);
+        memory[instr->arg0] = memory[instr->arg1] / memory[instr->arg2];
+        break;
+      case ASM_COP:
+        assert_params_in_bounds(instr, 2);
+        memory[instr->arg0] = memory[instr->arg1];
+        break;
+      case ASM_AFC:
+        assert_params_in_bounds(instr, 1);
+        memory[instr->arg0] = instr->arg1;
+        break;
+      case ASM_JMP:
+        pc = instr->arg0;
+        break;
+      case ASM_JMF:
+        assert_params_in_bounds(instr, 1);
+        if (memory[instr->arg0] == 0)
+          pc = instr->arg1;
+        break;
+      // TODO INF SUP EQU
+      case ASM_PRI:
+        assert_params_in_bounds(instr, 1);
+        printf("%d\n", memory[instr->arg0]);
+        break;
+      default:
+        fprintf(stderr, "unsupported op 0x%x at pc=0x%x\n", instr->op, pc);
+        return 2;
+    }
+  }
+
+  return 0;
+}
 
 int main() {
   FILE* in = fopen("test.foo.bytecode", "rb");
@@ -10,11 +85,14 @@ int main() {
 
   struct asm_table table;
   asm_init_table(&table);
-
   asm_read_bytecode(in, &table);
+  fclose(in);
+
   asm_fprintf(stdout, &table);
 
-  fclose(in);
+  printf("Running bytecode (%d instructions) from %s\n", table.instructions_count, "TODO");
+  run(&table);
+
   asm_free_table(&table);
   return 0;
 }
